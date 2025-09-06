87b3988f8237b54be2d6a255841643ddc53318c8
playercool <playercool02@gmail.com>
2025-09-06 10:32:38 +0530

    router and server setup init

--- Backend/cmd/synergy_api/main.go
+++ Backend/cmd/synergy_api/main.go
@@ -2,16 +2,34 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 	"synergy/internal/config"
 )
 
 func main() {
 	//config
-	var cfg config.Config
-	cfg = *config.Must_Load()
+	cfg := *config.Must_Load()
+
 	fmt.Print(cfg)
 	//setup routers
+	router := http.NewServeMux()
+	router.HandleFunc("GET /api/users", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		w.Write([]byte(`{"message":"List of students"}`))
+	})
+
 	//setup server
+	server := http.Server{
+		Addr:    cfg.Http_Server.Addr,
+		Handler: router,
+	}
+	done := make(chan os.Signal, 1)
+	go func() {
+		server.ListenAndServe()
+	}()
+	<-done
+
 	//storage setup
 
 }
