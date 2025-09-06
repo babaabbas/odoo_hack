9522018ceeb1f1b3216aa6226127cf22c191d3e4
playercool <playercool02@gmail.com>
2025-09-06 10:11:36 +0530

    added must_load function to load the config file

---
+++ Backend/internal/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env          string `yaml:"env" env-required:"true"`
+	Storage_Path string `yaml:"storage_path" env-required:"true"`
+	Http_Server  string `yaml:"http_server"`
+}
+
+type Http_Server struct {
+	Addr string `yaml:"address" env-required:"true"`
+}
+
+func Must_Load() *Config {
+	var config_path string
+	config_path = os.Getenv("config_path")
+	if config_path == "" {
+		flags := flag.String("config_path", "", "this is path for config_path")
+		flag.Parse()
+		config_path = *flags
+		if config_path == "" {
+			log.Fatal("path is empty for some reason, maybe its because you didnt send it as a flag")
+		}
+	}
+	if _, err := os.Stat(config_path); os.IsNotExist(err) {
+		log.Fatal("yo checked the file, its not present")
+	}
+	var cfg Config
+	err := cleanenv.ReadConfig(config_path, &cfg)
+	if err != nil {
+		log.Fatal("couldnt read the file")
+	}
+	return &cfg
+}
